cc34f848657cc7a2dc32e4abc56d0c7bd433df7a
nobo <505931977@qq.com>
2017-06-27 23:39:23 +0800

    Added      test/test/test1.html Modified   test/dist/test.js Modified   test/test.js

--- test/dist/test.js
+++ test/dist/test.js
@@ -22,6 +22,20 @@ var html2 = (0, _fs.readFileSync)('../table.html');
 
 var html2 = '<div>\n<p1 name="taf1"><span>test<i>1</i>...</span></p1>\n<p2 name="taf2">test2...</p2>\n<p3 name="taf3">test3...</p3>\n<p4 name="taf3">test4...</p4>\n<table>\n<js>\nfor( var i = 0; i < 100; i++ ) {\n    var s = i+ 4;\n</js>\n<tr>\n    <td>1</td>\n    <td>2</td>\n    <td>3</td>\n    <td>4</td>\n</tr>\n<js>\n}\n</js>\n</table>\n</div>';
 
+var html2 = '<div><s><i>1</id></s><js>var s1</js></div><br/><js>var s1</js>';
+
+function makeAttrsMap(attrs) {
+    var map = {};
+    for (var i = 0, l = attrs.length; i < l; i++) {
+        map[attrs[i].name] = attrs[i].value;
+    }
+    return map;
+}
+
+function isForbiddenTag(el) {
+    return el.tag === 'style' || el.tag === 'script' && (!el.attrsMap.type || el.attrsMap.type === 'text/javascript');
+}
+
 var str = [];
 
 var queue = [];
@@ -31,32 +45,34 @@ var queue = [];
 
 //var jsx = <div myname = {program + '}'} test>test<p>11</p></div>;
 
-var root = void 0;
+var root = void 0,
+    roots = [];
 
 var currentParent = void 0;
 
 var stack = [];
 
 var ret = (0, _htmlParser.parseHTML)(html2 + '', {
-    start: function start(tag, attr, unary) {
-        str.push((queue.length ? ',' : '') + ('h(' + tag + ',null'));
-        queue.push(tag);
-        console.log(tag, '...');
-        if (unary) {
-            str.push('null)');
-            queue.pop();
-        }
+    start: function start(tag, attrs, unary) {
         var element = {
             tag: tag,
             type: 1,
-            attrsList: attr,
-            //parent: currentParent,
+            attrsList: attrs,
+            attrsMap: makeAttrsMap(attrs),
+            parent: currentParent,
             children: []
         };
 
+        if (isForbiddenTag(element)) {
+            element.forbidden = true;
+        }
+
         if (currentParent) {
             currentParent.children.push(element);
-            //element.parent = currentParent;
+        }
+
+        if (stack.length === 0) {
+            roots.push(element);
         }
 
         if (!unary) {
@@ -69,21 +85,10 @@ var ret = (0, _htmlParser.parseHTML)(html2 + '', {
         }
     },
     end: function end() {
-        str.push(')');
-        queue.pop();
-
-        var element = stack[stack.length - 1];
-        //const lastNode = element.children[element.children.length - 1]
-        //if (lastNode && lastNode.type === 3 && lastNode.text === ' ') {
-        //    element.children.pop()
-        //}
         stack.length -= 1;
         currentParent = stack[stack.length - 1];
     },
     chars: function chars(text) {
-        str.push(',"' + text + '"');
-
-        //const children = currentParent.children
         text = text.trim();
         if (text) {
             currentParent.children.push({
@@ -94,58 +99,85 @@ var ret = (0, _htmlParser.parseHTML)(html2 + '', {
     }
 });
 
-function transform(ASTElement) {
-
-    var str = [];
-
+function transform(ASTElement, $root) {
+    var codes = [];
     var stack = [];
-    var stackAST = [];
+    //var stackAST = [];
 
-    function parser1(ast) {
-        var cid = '_c' + uuid++ + '_' + (ast.tag || 'text') + '_';
-        str.push('\nvar ' + cid + ' = [];');
+    function parser(ast) {
+        var isTextNode = ast.type === 3;
 
-        var parent = stack[stack.length - 1];
-        var parentAST = stackAST[stackAST.length - 1];
+        var $var = '_$var_' + (ast.tag || 'text') + ('_' + uuid++ + '_');
+        codes.push('var ' + $var + ' = [];');
 
-        stack.push(cid);
-        stackAST.push(ast);
+        var parentVar = stack[stack.length - 1] || $root;
+        //var parentAST = stackAST[stackAST.length - 1];
 
-        ast.children = ast.children || [];
+        stack.push($var);
+        //stackAST.push(ast);
 
-        ast.children.forEach(function (child, i) {
-            if (child.type === 3) {
-                str.push('\n' + cid + '.push(' + JSON.stringify(child.text) + ');');
-            } else if (child.type === 1) {
-                if (child.tag == 'js') {
-                    if (child.children[0]) {
-                        str.push('\n' + child.children[0].text);
+        if (!isTextNode) {
+
+            ast.children.forEach(function (child, i) {
+                if (child.type === 3) {
+                    codes.push($var + '.push(' + JSON.stringify(child.text) + ');');
+                } else if (child.type === 1) {
+                    if (child.tag == 'js') {
+                        if (child.children.length) {
+                            codes.push('' + child.children[0].text);
+                        }
+                    } else {
+                        parser(child);
                     }
-                } else {
-                    parser1(child);
                 }
-            }
-        });
+            });
+        }
 
         stack.length -= 1;
-        stackAST.length -= 1;
+        //stackAST.length -= 1;
 
-        if (stack.length) str.push('\n' + parent + '.push(h("' + ast.tag + '", null, ' + cid + '));');else str.push('\nh("' + ast.tag + '", null, ' + cid + ');');
+        //if( ast.tag === 'js' ) return;
+
+        //if( stack.length )
+        codes.push(parentVar + '.push(' + program + '("' + ast.tag + '", ' + JSON.stringify(ast.attrsMap) + ', ' + $var + '));');
+        //else 
+        // codes.push(`${program}("${ast.tag}", ${JSON.stringify(ast.attrsMap)}, ${$var});`);
     }
 
-    str.push(parser1(ASTElement));
+    parser(ASTElement);
 
-    return str.join('');
+    return codes.join('\n');
 }
 
-//console.log(root);
+function compile(ASTElements) {
+    var $var = '_temp_' + uuid++ + '_';
+    var codes = ['var ' + $var + ' = [];'];
 
-//console.log(JSON.stringify(root));
+    for (var i = 0; i < ASTElements.length; i++) {
+        var ast = ASTElements[i];
+        if (ast.type === 3) {
+            codes.push($var + '.push(' + ast.text + ')');
+        } else if (ast.type === 1 && ast.tag === 'js') {
+            if (ast.children.length) {
+                codes.push('' + ast.children[0].text);
+            }
+        } else {
+            codes.push(transform(ast, $var));
+        }
+    }
+
+    codes.push('return ' + $var);
+
+    return new Function('props', 'state', 'context', codes.join('\n'));
+}
 
 var uuid = 1;
 var ASTElement = root;
+
+//console.log(roots);
+
 //transform
-console.log(transform(root));
+console.log(compile(roots).toString());
 
 /**
  * <div id="t1"><p>test...</p></div>
@@ -157,3 +189,58 @@ console.log(transform(root));
  * h('div', {id:'t1'}, v1);
  * ```
 */
+
+function includes(text, sep) {
+    return text.indexOf(sep) >= 0;
+}
+
+var curlyMap = { '{': 1, '}': -1 };
+
+console.log(convertText({
+    type: 'text'
+}, null, 'adf{2+1 + "{}" }fg'));
+
+function isStringOnlyCode(txt) {
+    return (/^\s*\{.*}\s*$/g.test(txt)
+    );
+    //txt = txt.trim();
+    //return txt.length && txt.charAt(0) === '{' && txt.charAt(txt.length - 1) === '}';
+}
+
+function convertText(node, context, txt, normalizeWhitespaces) {
+    function jsonText(text) {
+        return JSON.stringify(normalizeWhitespaces ? normalizeHtmlWhitespace(text) : text);
+    }
+    var res = '';
+    var first = true;
+    var concatChar = node.type === 'text' ? ',' : '+';
+
+    while (includes(txt, '{')) {
+        var start = txt.indexOf('{');
+        var pre = txt.substr(0, start);
+        if (pre) {
+            res += (first ? '' : concatChar) + jsonText(pre);
+            first = false;
+        }
+        var curlyCounter = 1;
+        var end = start;
+        while (++end < txt.length && curlyCounter > 0) {
+            curlyCounter += curlyMap[txt.charAt(end)] || 0;
+        }
+        if (curlyCounter === 0) {
+            var needsParens = start !== 0 || end !== txt.length - 1;
+            res += (first ? '' : concatChar) + (needsParens ? '(' : '') + txt.substr(start + 1, end - start - 2) + (needsParens ? ')' : '');
+            first = false;
+            txt = txt.substr(end);
+        } else {
+            //throw RTCodeError.build(context, node, `Failed to parse text '${txt}'`);
+        }
+    }
+    if (txt) {
+        res += (first ? '' : concatChar) + jsonText(txt);
+    }
+    if (res === '') {
+        res = 'true';
+    }
+    return res;
+}
--- test/test.js
+++ test/test.js
@@ -38,6 +38,27 @@ for( var i = 0; i < 100; i++ ) {
 </table>
 </div>`;
 
+var html2 = `<div><s><i>1</id></s><js>var s1</js></div><br/><js>var s1</js>`
+
+function makeAttrsMap (attrs) {
+  const map = {}
+  for (let i = 0, l = attrs.length; i < l; i++) {
+    map[attrs[i].name] = attrs[i].value
+  }
+  return map
+}
+
+function isForbiddenTag (el) {
+  return (
+    el.tag === 'style' ||
+    (el.tag === 'script' && (
+      !el.attrsMap.type ||
+      el.attrsMap.type === 'text/javascript'
+    ))
+  )
+}
+
+
 var str = [];
 
 let queue = [];
@@ -47,32 +68,33 @@ let queue = [];
 
 //var jsx = <div myname = {program + '}'} test>test<p>11</p></div>;
 
-let root;
+let root, roots=[];
 
 let currentParent;
 
 let stack = [];
 
 const ret = parseHTML(html2 + '', {
-    start: function(tag, attr, unary) {
-        str.push((queue.length ? ',' : '') + `h(${tag},null`);
-        queue.push(tag);
-        console.log(tag,'...');
-        if (unary) {
-            str.push(`null)`);
-            queue.pop();
-        }
+    start: function(tag, attrs, unary) {
         let element = {
             tag: tag,
             type: 1,
-            attrsList: attr,
-            //parent: currentParent,
+            attrsList: attrs,
+            attrsMap: makeAttrsMap(attrs),
+            parent: currentParent,
             children: []
+        }
+
+        if (isForbiddenTag(element)) {
+            element.forbidden = true;
         }
 
         if (currentParent) {
             currentParent.children.push(element)
-                //element.parent = currentParent;
+        }
+
+        if(stack.length === 0) {
+            roots.push(element);
         }
 
         if (!unary) {
@@ -85,21 +107,10 @@ const ret = parseHTML(html2 + '', {
         }
     },
     end: function() {
-        str.push(`)`);
-        queue.pop();
-
-        const element = stack[stack.length - 1];
-        //const lastNode = element.children[element.children.length - 1]
-        //if (lastNode && lastNode.type === 3 && lastNode.text === ' ') {
-        //    element.children.pop()
-        //}
         stack.length -= 1;
         currentParent = stack[stack.length - 1];
     },
     chars: function(text) {
-        str.push(`,"${text}"`);
-
-        //const children = currentParent.children
         text = text.trim();
         if (text) {
             currentParent.children.push({
@@ -110,61 +121,86 @@ const ret = parseHTML(html2 + '', {
     }
 });
 
-function transform(ASTElement){
-
-    var str= [];
-
+function transform(ASTElement, $root){
+    var codes= [];
     var stack = [];
-    var stackAST = [];
+    //var stackAST = [];
 
-    function parser1(ast){
-        var cid =  `_c${uuid++}_` + ( ast.tag || 'text' ) + '_';
-        str.push(`\nvar ${cid} = [];`);
+    function parser(ast){
+        var isTextNode = ast.type === 3;
 
-        var parent = stack[stack.length - 1];
-        var parentAST = stackAST[stackAST.length - 1];
+        var $var =  '_$var_' + ( ast.tag || 'text' ) + `_${uuid++}_`;
+        codes.push(`var ${$var} = [];`);
 
-        stack.push(cid);
-        stackAST.push(ast);
+        var parentVar = stack[stack.length - 1] || $root;
+        //var parentAST = stackAST[stackAST.length - 1];
 
-        ast.children = ast.children || [];
+        stack.push($var);
+        //stackAST.push(ast);
 
-        ast.children.forEach((child, i)=>{
-            if( child.type === 3 ) {
-                str.push(`\n${cid}.push(${JSON.stringify(child.text)});`); 
-            } else if( child.type === 1 ) {
-                if( child.tag == 'js' ) {
-                    if( child.children[0] ) {
-                        str.push(`\n${child.children[0].text}`);
+        if( !isTextNode ) {
+
+            ast.children.forEach((child, i)=>{
+                if( child.type === 3 ) {
+                    codes.push(`${$var}.push(${JSON.stringify(child.text)});`); 
+                } else if( child.type === 1 ) {
+                    if( child.tag == 'js' ) {
+                        if( child.children.length ) {
+                            codes.push(`${child.children[0].text}`);
+                        }
+                    } else {
+                        parser(child);
                     }
-                } else {
-                    parser1(child);
                 }
-            }
-        });
+            });
+
+        }
 
         stack.length -= 1;
-        stackAST.length -= 1;
+        //stackAST.length -= 1;
 
-        if( stack.length )
-            str.push(`\n${parent}.push(h("${ast.tag}", null, ${cid}));`);
-        else 
-            str.push(`\nh("${ast.tag}", null, ${cid});`);
+        //if( ast.tag === 'js' ) return;
+
+        //if( stack.length )
+            codes.push(`${parentVar}.push(${program}("${ast.tag}", ${JSON.stringify(ast.attrsMap)}, ${$var}));`);
+        //else 
+           // codes.push(`${program}("${ast.tag}", ${JSON.stringify(ast.attrsMap)}, ${$var});`);
     }
 
-    str.push(parser1(ASTElement));
+    parser(ASTElement);
 
-    return str.join('');
+    return codes.join('\n');
 }
 
-//console.log(root);
+function compile(ASTElements){
+    var $var = `_temp_${uuid++}_`;
+    var codes = [`var ${$var} = [];`];
 
-//console.log(JSON.stringify(root));
+    for( var i=0;i<ASTElements.length;i++ ) {
+        var ast = ASTElements[i];
+        if( ast.type === 3 ) {
+            codes.push(`${$var}.push(${ast.text})`);
+        } else if( ast.type === 1 && ast.tag === 'js' ) {
+             if( ast.children.length ) {
+                codes.push(`${ast.children[0].text}`);
+             }
+        } else {    
+            codes.push(transform(ast, $var));
+        }
+    }
+
+    codes.push( `return ${$var}` );
+
+    return new Function('props', 'state', 'context', codes.join('\n')) ;
+}
 
 let uuid = 1;
 let ASTElement = root;
+
+//console.log(roots);
+
 //transform
-console.log(transform(root));
+console.log(compile(roots).toString());
 
 /**
  * <div id="t1"><p>test...</p></div>
@@ -176,3 +212,57 @@ console.log(transform(root));
  * h('div', {id:'t1'}, v1);
  * ```
 */
+
+function includes(text, sep) {
+    return text.indexOf(sep) >= 0;
+}
+
+const curlyMap = {'{': 1, '}': -1};
+
+console.log( convertText({
+    type : 'text'
+},null,'adf{2+1 + "{}" }fg') );
+
+function isStringOnlyCode(txt) {
+    return /^\s*\{.*}\s*$/g.test(txt);
+    //txt = txt.trim();
+    //return txt.length && txt.charAt(0) === '{' && txt.charAt(txt.length - 1) === '}';
+}
+
+function convertText(node, context, txt, normalizeWhitespaces) {
+    function jsonText(text) {
+        return JSON.stringify(normalizeWhitespaces ? normalizeHtmlWhitespace(text) : text);
+    }
+    let res = '';
+    let first = true;
+    const concatChar = node.type === 'text' ? ',' : '+';
+ 
+    while (includes(txt, '{')) {
+        const start = txt.indexOf('{');
+        const pre = txt.substr(0, start);
+        if (pre) {
+            res += (first ? '' : concatChar) + jsonText(pre);
+            first = false;
+        }
+        let curlyCounter = 1;
+        let end = start;
+        while (++end < txt.length && curlyCounter > 0) {
+            curlyCounter += curlyMap[txt.charAt(end)] || 0;
+        }
+        if (curlyCounter === 0) {
+            const needsParens = start !== 0 || end !== txt.length - 1;
+            res += (first ? '' : concatChar) + (needsParens ? '(' : '') + txt.substr(start + 1, end - start - 2) + (needsParens ? ')' : '');
+            first = false;
+            txt = txt.substr(end);
+        } else {
+            //throw RTCodeError.build(context, node, `Failed to parse text '${txt}'`);
+        }
+    }
+    if (txt) {
+        res += (first ? '' : concatChar) + jsonText(txt);
+    }
+    if (res === '') {
+        res = 'true';
+    }
+    return res;
+}
